d39e271d4a027c6ba5e858bcb15a9c763cf940be
Slacker87 <ccnp_pro@yahoo.com>
2016-01-21 20:23:37 +0000

    finish basic site

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
   def index
+      @entries = User.pluck(:username, :entry)
+  end
+  def entry
+  end
+  def newentry
+      @user = current_user
+      @user.entry = params[:entry][:entry]
+      if @user.save
+        flash[:notice] = "Successfully entered!"
+        return redirect_to root_path
+      else
+          flash[:error] = "Failed to save entry, please try again"
+          return render :entry
+      end
   end
 end
